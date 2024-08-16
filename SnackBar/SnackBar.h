d383617de0a10d5c98a61453baf3852061957b9d
LiamHsu <s459958@gmail.com>
2024-08-16 11:34:44 +0800

    Init xcode project

---
+++ SnackBar/SnackBar.h
@@ -0,0 +1,18 @@
+//
+//  SnackBar.h
+//  SnackBar
+//
+//  Created by Liam on 2024/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SnackBar.
+FOUNDATION_EXPORT double SnackBarVersionNumber;
+
+//! Project version string for SnackBar.
+FOUNDATION_EXPORT const unsigned char SnackBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnackBar/PublicHeader.h>
+
+
