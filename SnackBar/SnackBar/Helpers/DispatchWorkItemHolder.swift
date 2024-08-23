c655770a24a48abf04692b1582606474bb645ca8
LiamHsu <s459958@gmail.com>
2024-08-23 09:26:59 +0800

    Move DispatchWorkItemHolder to its own file

---
+++ SnackBar/SnackBar/Helpers/DispatchWorkItemHolder.swift
@@ -0,0 +1,12 @@
+//
+//  DispatchWorkItemHolder.swift
+//  SnackBar
+//
+//  Created by Liam on 2024/8/23.
+//
+
+import Foundation
+
+final class DispatchWorkItemHolder {
+    var work: DispatchWorkItem?
+}
