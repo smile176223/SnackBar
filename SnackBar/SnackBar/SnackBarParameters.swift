7cd13e314bdc2b349b0687f410ab50e5abef4571
LiamHsu <s459958@gmail.com>
2024-08-23 14:09:47 +0800

    Move animation control in parameters and add comment

--- SnackBar/SnackBar/SnackBarParameters.swift
+++ SnackBar/SnackBar/SnackBarParameters.swift
@@ -5,7 +5,7 @@
 //  Created by Liam on 2024/8/23.
 //
 
-import Foundation
+import SwiftUI
 
 public struct SnackBarParameters {
     
@@ -14,19 +14,29 @@ public struct SnackBarParameters {
         case bottom
     }
     
+    /// Control SnackBar slide position
     public var position: Position = .bottom
+    /// Display position include padding
     public var padding: CGFloat = 10
-    
-    
-    public func position(_ position: Position) -> SnackBarParameters {
+    /// Control SnackBar animation type
+    public var animation: Animation = .spring()
+}
+
+// MARK: - Update Method
+extension SnackBarParameters {
+    public func position(_ position: Position) -> Self {
         updateParameters { $0.position = position }
     }
     
-    public func padding(_ padding: CGFloat) -> SnackBarParameters {
+    public func padding(_ padding: CGFloat) -> Self {
         updateParameters { $0.padding = padding }
     }
     
-    private func updateParameters(_ update: (inout SnackBarParameters) -> Void) -> SnackBarParameters {
+    public func animation(_ animation: Animation) -> Self {
+        updateParameters { $0.animation = animation }
+    }
+    
+    private func updateParameters(_ update: (inout Self) -> Void) -> Self {
         var parameters = self
         update(&parameters)
         return parameters
